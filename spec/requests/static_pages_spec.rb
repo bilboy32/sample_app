5bb89900faa3c10fb7aa8b3a2d41432e55fd5d1d
William Pilkonis <w.pilkonis@gmail.com>
2013-10-23 18:51:08 -0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,21 @@ describe "Static pages" do
     it { should have_title(full_title(page_title)) }
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title('Welcome to the Sample App'))
+  end
+
   describe "Home page" do
     before { visit root_path }
     let(:heading)    { 'Sample App' }
@@ -38,22 +53,7 @@ describe "Static pages" do
     before { visit contact_path }
     let(:heading)    { 'Contact' }
     let(:page_title) { '' }
-
+  
     it_should_behave_like "all static pages"
   end
-
-    it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    expect(page).to have_title(full_title('About Us'))
-    click_link "Help"
-    expect(page).to have_title(full_title('Help'))
-    click_link "Contact"
-    expect(page).to have_title(full_title('Contact'))
-    click_link "Home"
-    click_link "Sign up now!"
-    expect(page).to have_title(full_title('Sign Up'))
-    click_link "sample app"
-    expect(page).to have_title(full_title('Welcome to the Sample App'))
-    end
-  end
+end
